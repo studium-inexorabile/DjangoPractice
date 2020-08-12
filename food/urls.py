003c49de2d3b628bf90eb6ca847962ffa248d3b7
studium-inexorabile <68911602+studium-inexorabile@users.noreply.github.com>
2020-08-12 16:57:10 -0500

    static files / forms

--- food/urls.py
+++ food/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.detail,name='detail'),
     path('item/', views.item, name='item'),
+    path('add/', views.create_item, name='create_item'),
+    path('update/<int:item_id>', views.update_item, name='update_item'),
+    path('delete/<int:item_id>', views.delete_item, name='delete_item'),
 ]
