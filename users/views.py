19cf9721bf232134d4e14a1ec2303c009828cb58
studium-inexorabile <68911602+studium-inexorabile@users.noreply.github.com>
2020-08-15 16:17:29 -0500

    added auth

--- users/views.py
+++ users/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username}!')
+            return redirect('login')
+    form = RegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profilePage(request):
+    return render(request, 'users/profile.html')
