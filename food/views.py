2416f7738b8c88c18ce717ac3b4162b7fc3fac8a
studium-inexorabile <68911602+studium-inexorabile@users.noreply.github.com>
2020-08-17 16:19:42 -0500

    authentication / signals

--- food/views.py
+++ food/views.py
@@ -3,33 +3,44 @@ from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
-def index(request):
-    item_list = Item.objects.all()
-    # template = loader.get_template('food/index.html')
-    context = {
-        'item_list': item_list,
-    }
-    return render(request, 'food/index.html', context)
+# def index(request):
+#     item_list = Item.objects.all()
+#     context = {
+#         'item_list': item_list,
+#     }
+#     return render(request, 'food/index.html', context)
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = "food/index.html"
+    context_object_name = 'item_list'
 
 def item(request):
     return HttpResponse('<h2>This is an item view</h2>')
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item':item,
-    }
-    return render(request, 'food/detail.html', context)
+# def detail(request, item_id):
+#     item = Item.objects.get(pk=item_id)
+#     context = {
+#         'item':item,
+#     }
+#     return render(request, 'food/detail.html', context)
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+
 
 def create_item(request):
     form = ItemForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('food:index')
-    
+
     return render(request, 'food/item-form.html', {'form':form})
 
 def update_item(request, item_id):
@@ -38,7 +49,7 @@ def update_item(request, item_id):
     if form.is_valid():
         form.save()
         return redirect('food:index')
-    
+
     return render(request, 'food/item-form.html', {'form':form, 'item':item})
 
 def delete_item(request, item_id):
