17fa06cc479ef32214817c629ed15cf5479a78a6
studium-inexorabile <68911602+studium-inexorabile@users.noreply.github.com>
2020-08-20 21:28:33 -0500

    added views for creating item tied to user

--- food/urls.py
+++ food/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.IndexClassView.as_view(), name='index'),
     path('<int:pk>/', views.ItemDetailView.as_view(),name='detail'),
     path('item/', views.item, name='item'),
-    path('add/', views.create_item, name='create_item'),
+    path('add/', views.CreateItem.as_view(), name='create_item'),
     path('update/<int:item_id>', views.update_item, name='update_item'),
     path('delete/<int:item_id>', views.delete_item, name='delete_item'),
 ]
--- food/views.py
+++ food/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from .forms import ItemForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
@@ -42,6 +43,16 @@ def create_item(request):
         return redirect('food:index')
 
     return render(request, 'food/item-form.html', {'form':form})
+
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_description', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+
+        return super().form_valid(form)
+    
 
 def update_item(request, item_id):
     item = Item.objects.get(id=item_id)
